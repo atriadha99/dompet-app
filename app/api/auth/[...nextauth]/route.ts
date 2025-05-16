c8a892fec3bf5411a68ef6bd2f4295b01fd52d72
Andika Agung <atriadha@gmail.com>
2025-05-16 17:09:51 +0700

    import and deleted files

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,63 @@
+// app/api/auth/[...nextauth]/route.ts
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../lib/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+import type { AuthOptions, SessionStrategy } from "next-auth";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        if (!user) return null;
+
+        // Verifikasi password
+        const isValidPassword = await bcrypt.compare(
+          credentials?.password || "",
+          user.password
+        );
+
+        if (!isValidPassword) return null;
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+    error: "/auth/error",
+  },
+  session: {
+    strategy: "jwt" as SessionStrategy,
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      if (user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
